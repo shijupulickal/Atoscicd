ad7cb1672dbc32369d3ef4e751c4d97836ef9b0d
shijupulickal <shiju500@gmail.com>
2024-11-11 21:36:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
 
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.102.0"
     }
   }
 }
+
+provider "azurerm" {
+ features {}
+}
+
+
 
 /*
 terraform {
